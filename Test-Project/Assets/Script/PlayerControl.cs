e653b646b57185ff56ad9e6a944bae4145bfd428
TCWen <chunwen@acodes.com.my>
2021-03-05 17:39:07 +0800

    player control sideways

--- Test-Project/Assets/Script/PlayerControl.cs
+++ Test-Project/Assets/Script/PlayerControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public Rigidbody rb;
+    public float forewardmov = 500;
+    public float sidemov = 250;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,15 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        rb.AddForce(-forewardmov * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, -sidemov);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, sidemov);
+        }
     }
 }
