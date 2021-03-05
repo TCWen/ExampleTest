d8f57fe63127e8bbb7b95717537ff66ceed89e08
TCWen <chunwen@acodes.com.my>
2021-03-05 16:13:26 +0800

    player movement

---
+++ Test-Project/Assets/Script/PlayerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(-500 * Time.deltaTime, 0, 0);
+    }
+}
